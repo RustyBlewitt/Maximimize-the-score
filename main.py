2a2bb639a19238a75127b3ee42ebd1e63f0b4e7f
Rusty Blewitt <rusty.blewitt@griffithuni.edu.au>
2019-05-02 23:23:22 +1000

    Object oriented implementation now working with correct results

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-# Maximize the score #
-# 2801ICT Project 2 - s5131071 - Rusty Blewitt #
-
 import sys  # Get command line args
 import os   # Get cwd
-from priority_q import scott_q, rusty_q
+from ball_and_queues import Ball, PriorityQ, DigitValuePriorityQ
 
-
-# Opening of input file ------------
+# Opening of input file ----
 try:
     fname = sys.argv[1]
 except IndexError:
@@ -19,52 +15,40 @@ except FileNotFoundError :
     print("The file '%s' could not be opened, please check filename and path " %fname +
             "relative to %s then try again." %os.getcwd())
     exit(1)
-#-----------------------------------
-
+#----------------------------
 
 T = int(f.readline())                   # Number of test cases
-print("Test cases: ", T)
 
 for t in range(T):                      # For each test case...
-    # print("\n\n")
-    scotts_q = scott_q()                            # Init priority queues
-    rustys_q = rusty_q()
 
-    line_one = f.readline().split()                 # Input 1/3 : Amount of balls, and turns allowed each round
-    ball_count = int(line_one[0])
+    scotts_q = PriorityQ()                 # Init priority queues
+    rustys_q = DigitValuePriorityQ()       #
+
+    line_one = f.readline().split()     # Input 1/3 : Amount of balls, and turns allowed each round
+    ball_count = int(line_one[0])   
     turn_count = int(line_one[1])
 
-    balls = [int(b) for b in f.readline().split()]  # Input 2/3 : Available balls
-    for b in balls:
-        scotts_q.enqueue(int(b))
-        rustys_q.enqueue(int(b))
+    for n in f.readline().split():      # Input 2/3 : Available balls
+        b = Ball(int(n))                    # Init ball object
+        scotts_q.enqueue(b)                 # Pass a reference of that object to Scott's queue
+        rustys_q.enqueue(b)                 # Pass a reference of that object to Rusty's queue
 
-    toss = f.readline()                             # Input 3/3 : Result of the toin coss
-    scotts_turn = 'HEADS' in toss
+    toss = f.readline()                 # Input 3/3 : Result of the toin coss
 
-    scotts_score = 0                                # Init scores
-    rustys_score = 0
-    
+    # Current player is Rusty if toin coss is tails, otherwise Scott
+    player = rustys_q if 'TAILS' in toss else scotts_q
 
-    for turn in range(ball_count):                  
-        # print(turn)
-        if (turn % turn_count == 0 and turn > 0):
-            scotts_turn = not scotts_turn
-        
-        if scotts_turn:     # Scotts turn
-            pick = scotts_q.dequeue()
-            while(pick not in balls):               # Pick again if ball already used
-                pick = scotts_q.dequeue()
-            balls.remove(pick)
-            scotts_score += pick
-            # print("Scotts turn. Picked %d. Score now %d" %(pick, scotts_score))
-        
-        else:               # Rustys turn
-            pick = rustys_q.dequeue()
-            while(pick not in balls):              # Pick again if ball already used
-                pick = rustys_q.dequeue()
-            balls.remove(pick)
-            rustys_score += pick 
-            # print("Rustys turn. Picked %d. Score now %d" %(pick, rustys_score))
-    
-    print("%d %d" %(scotts_score, rustys_score))
+    for turn in range(1, ball_count+1): # For each turn (each unique ball)
+        while( len(player.nodes) > 0 ):     # While player still has ball references in their queue
+            pick = player.dequeue()             # Pick highest priority ball for that player
+
+            if not pick.used:                   # If ball picked hasn't yet been picked
+                player.score += pick.real_val   # Player adds that balls value to their score
+                pick.used = True                # Ball object is marked as having been picked
+                break
+
+        if (turn % turn_count == 0):        # If next player's turn next, switch current player
+            player = scotts_q if player == rustys_q else rustys_q
+
+
+    print("%d %d" %(scotts_q.score, rustys_q.score))
